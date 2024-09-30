e4f15763776e3811eea865b3a12a45a1ded59a7b
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-09-30 12:00:03 -0600

    first POST request with go gin

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func getAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
@@ -31,4 +32,19 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// postAlbums adds an album from JSON received in the request body.
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
